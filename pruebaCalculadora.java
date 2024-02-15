1202fa9de5eec6654233798b7d21ed7ef23474f2
Agustín Rubí Hernández <surviiplay@gmail.com>
2024-02-15 20:00:01 +0100

    Agregada la clase pruebaCalculadora

---
+++ pruebaCalculadora.java
@@ -0,0 +1,6 @@
+public class pruebaCalculadora {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora(10, 5);
+        System.out.println("Suma: " + calc.suma());
+    }
+}
